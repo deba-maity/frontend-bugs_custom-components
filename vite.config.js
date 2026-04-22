1e091f626c0b565b375b42153f75f05abe3e43cc
deba-maity <debajyoti.maity29@gmail.com>
2026-04-22 10:58:54 +0530

    deploy changes

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 4173,
-    host: "0.0.0.0"
-  }
+    host: "0.0.0.0",
+  },
+  base: "/frontend-bugs_custom-components/",
 });
